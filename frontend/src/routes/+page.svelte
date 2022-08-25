c73607bf2237a756ebf210253b5e6d8b1d9ab274
Paul Macey <register@paulmacey.com>
2022-08-25 15:52:38 +1000

    create frontend pages

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,1 +1,11 @@
-<p>Welcome</p>
+<script>
+	let count = 0;
+
+</script>
+
+<div class="bg-white py-6 sm:py-8 lg:py-12">
+	<div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+		<p class="text-indigo-500 lg:text-lg font-semibold text-center mb-2 md:mb-3">Scratch</p>
+		<p class="" >A simple note taking app</p>
+	</div>
+</div>
